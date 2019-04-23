726d67ac9bb3868babda9ed967b7f203c596ec9e
Scott Hanselman <scott@hanselman.com>
2019-04-23 12:48:10 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace emojimadness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            var f1 = new string[]{ "🌑", "🌒", "🌓", "🌔", "🌝", "🌖", "🌗", "🌘" };
+            var f2 = new string[]{ "🕐", "🕑", "🕒", "🕓", "🕔", "🕕", "🕖", "🕗", "🕘", "🕙", "🕚", "🕛"};
+            var f3 = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+            foreach (string s in f1)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(s + "    ");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            foreach (string s in f2)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(s + "    ");
+                Thread.Sleep(100);
+            }
+            Console.WriteLine();
+            foreach (string s in f3)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop);
+                Console.Write(s + "    ");
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
